8450d2ff0b7c0fb0ae2b1d7d94c6c330197a0441
VidathAmarasekara <vidathamarasekara99@gmail.com>
2024-03-16 21:05:56 +0530

    Get function

--- controller/kandyanController.js
+++ controller/kandyanController.js
@@ -38,4 +38,15 @@ const postImage = async (req, res) => {
   }
 };
 
-module.exports = { postImage };
+const getKandyanData = async (req, res) => {
+  try {
+    const kandyanData = await Kandyan.find({}).collation({}); // Fetch all data from the Kandyan collection
+
+    res.status(200).json(kandyanData);
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+
+module.exports = { postImage, getKandyanData };
--- route/kandyanRoute.js
+++ route/kandyanRoute.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const connectDatabase = require("../dbConnection");
 const {
-    postImage,
+    postImage, getKandyanData
 } = require("../controller/kandyanController");
 
 router.post("/postImage",  postImage);
+router.get("/getKandyanData",  getKandyanData);
 
 
 module.exports = router;
