35a7563ce2c081c806a7e26f6ec5d7b26ea09d6e
VidathAmarasekara <vidathamarasekara99@gmail.com>
2024-03-18 09:27:51 +0530

    Minor Changes

--- controller/kandyanController.js
+++ controller/kandyanController.js
@@ -7,30 +7,41 @@ const KandyanInfo = mongoose.model("KandyanInfo");
 // POST function to handle image upload
 
 const uploadImage = async (req, res) => {
-  const { modelno, layer1Img } = req.body;
+  const { modelno, layer1Img, layer2Img, layer3Img } = req.body;
 
   try {
-    if (!layer1Img) {
-      return res.status(400).json({ error: "Please provide three images" });
+    if (!layer1Img || !layer2Img || !layer3Img) {
+      return res.status(400).json({ error: "Please provide all three images" });
     }
-    const result = await cloudinary.uploader.upload(layer1Img, {
-      folder: "kandyan",
-      // width: 300,
-      // crop: "scale"
-    });
-    const kandyan = await KandyanInfo.create({
-      modelno,
-      layer1Img: {
+
+    const uploadPromises = [layer1Img, layer2Img, layer3Img].map(async (img) => {
+      const result = await cloudinary.uploader.upload(img, {
+        folder: "kandyan",
+        // width: 300,
+        // crop: "scale"
+      });
+      return {
         public_id: result.public_id,
         url: result.secure_url,
-      }
+      };
     });
+
+    const [layer1, layer2, layer3] = await Promise.all(uploadPromises);
+
+    const kandyan = await KandyanInfo.create({
+      modelno,
+      layer1Img: layer1,
+      layer2Img: layer2,
+      layer3Img: layer3,
+    });
+
     res.send({ status: "ok" });
   } catch (error) {
-    console.error("Error uploading image:", error);
-    res.status(500).send({ error: "An error occurred while uploading the image." });
+    console.error("Error uploading images:", error);
+    res.status(500).send({ error: "An error occurred while uploading the images." });
   }
 };
+
 
 const getKandyanData = async (req, res) => {
   try {
--- model/kandyan.js
+++ model/kandyan.js
@@ -6,8 +6,27 @@ const KandyanScehma = new mongoose.Schema(
       type: String,
       required: true,
     },
-    
     layer1Img: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+    layer2Img: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+    layer3Img: {
       public_id: {
         type: String,
         required: true,
